299db3ffe15837a4b3c7835dae6eacc6abae3e1b
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2023-11-14 14:35:49 +0100

    new changes

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./index.css";
 import { BsLightningCharge } from "react-icons/bs";
 import AOS from "aos";
@@ -6,38 +6,230 @@ import "aos/dist/aos.css";
 import { Bounce } from "react-awesome-reveal";
 
 const About = () => {
- 
+  const firstTextRef = useRef(null);
+  const secondTextRef = useRef(null);
+  const thirdTextRef = useRef(null);
+  const btnRef = useRef(null);
+  const avatarRef = useRef(null);
+  const nameRef = useRef(null);
 
+  const [firstTextVisible, setFirstTextVisible] = useState(false);
+  const [secondTextVisible, setSecondTextVisible] = useState(false);
+  const [thirdTextVisible, setThirdTextVisible] = useState(false);
+  const [btnVisible, setBtnVisible] = useState(false);
+  const [avatarVisible, setAvatarVisible] = useState(false);
+  const [nameVisible, setNameVisible] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (firstTextRef.current && !firstTextVisible) {
+        const top = firstTextRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setFirstTextVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [firstTextVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (secondTextRef.current && !secondTextVisible) {
+        const top = secondTextRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setSecondTextVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [secondTextVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (thirdTextRef.current && !thirdTextVisible) {
+        const top = thirdTextRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setThirdTextVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [thirdTextVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (btnRef.current && !btnVisible) {
+        const top = btnRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setBtnVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [btnVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (avatarRef.current && !avatarVisible) {
+        const top = avatarRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setAvatarVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [avatarVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (nameRef.current && !nameVisible) {
+        const top = nameRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setNameVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [nameVisible]);
   return (
     <div className="about">
       <div className="avatar">
-        <img src="/Avatar.png" alt="" className="" />
-        <p className="pressura">P.SENAKA</p>
+        <img src="/Avatar.png" alt="" 
+         ref={avatarRef}
+         className={`bottom-text-about animate-highlight ${
+           avatarVisible ? "animated-fade-down-texts" : ""
+         }`}
+        />
+        <p 
+         ref={nameRef}
+         className={`pressura ${
+           nameVisible ? "animated-fade-down-textss" : ""
+         }`}
+        >P.SENAKA</p>
       </div>
       <div className="bottom-line"></div>
 
-      <Bounce>
-        <p className="bottom-text-about animate-highlight">
-          For over a decade, I’ve worked at the intersection of design,
-          behavioural science and technology. Helping companies like Airbnb,
-          Google, PayPal & Fitbit execute intelligent and elegant products.
-        </p>
-      </Bounce>
-      <Bounce>
-        <p className="bottom-text-about  animate-highlight">
+      <p
+        ref={firstTextRef}
+        className={`bottom-text-about animate-highlight ${
+          firstTextVisible ? "animated-fade-down-texts" : ""
+        }`}
+      >
+        For over a decade, I’ve worked at the intersection of design,
+        behavioural science and technology. Helping companies like Airbnb,
+        Google, PayPal & Fitbit execute intelligent and elegant products.
+      </p>
+        <p 
+        ref={secondTextRef}
+        className={`bottom-text-about animate-highlight ${
+          secondTextVisible ? "animated-fade-down-texts" : ""
+        }`}
+        >
           Over a period of travel, I’ve experienced the cultures and
           perspectives of people in over 90 countries. These interactions have
           deepened my understanding of global injustices and shifted my focus
           toward projects that tackle today’s most pressing issues.
         </p>
-      </Bounce>
 
       <div className="about-bottom">
         <div className="bottom-line second-bottom-line"></div>
-        <p className="bottom-text animate-highlight">
+        <p 
+          ref={thirdTextRef}
+          className={`bottom-text animate-highlight ${
+            thirdTextVisible ? "animated-fade-down-texts" : ""
+          }`}
+        >
           Available for 40 hours a month. One project at a time. Zero Cost.
         </p>
-        <div className="bottom-btn">
+        <div 
+         ref={btnRef}
+         className={`bottom-btn ${
+           btnVisible ? "animated-fade-down-short" : ""
+         }`}
+        >
           <BsLightningCharge className="charge-btn" />
           <p className="bottom-btn-text start">Start Something</p>
         </div>
--- src/components/AnimatedText/index.jsx
+++ src/components/AnimatedText/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./index.css";
 import { ScrollText } from "./ScrollText";
 import { BsLightningCharge } from "react-icons/bs";
@@ -19,6 +19,286 @@ const AnimatedText = () => {
     "kindness is truly revolutionary.",
   ];
 
+  const [isVisible, setIsVisible] = useState(false);
+  const [btnVisible, setBtnVisible] = useState(false);
+  const [strategyVisible, setStrategyVisible] = useState(false);
+  const [strategyTextVisible, setStrategyTextVisible] = useState(false);
+  const [messageVisible, setMessageVisible] = useState(false);
+  const [messageTextVisible, setMessageTextVisible] = useState(false);
+  const [designVisible, setDesignVisible] = useState(false);
+  const [designTextVisible, setDesignTextVisible] = useState(false);
+  const [ideationVisible, setIdeationVisible] = useState(false);
+  const [ideationTextVisible, setIdeationTextVisible] = useState(false);
+  const sectionRef = useRef(null);
+  const btnRef = useRef(null);
+  const strategyRef = useRef(null);
+  const strategyTextRef = useRef(null);
+  const messageRef = useRef(null);
+  const messageTextRef = useRef(null);
+  const designRef = useRef(null);
+  const designTextRef = useRef(null);
+  const ideationRef = useRef(null);
+  const ideationTextRef = useRef(null);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (sectionRef.current && !isVisible) {
+        const top = sectionRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setIsVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (btnRef.current && !btnVisible) {
+        const top = btnRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setBtnVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [btnVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (strategyRef.current && !strategyVisible) {
+        const top = strategyRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setStrategyVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [strategyVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (strategyTextRef.current && !strategyTextVisible) {
+        const top = strategyTextRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setStrategyTextVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [strategyTextVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (messageRef.current && !messageVisible) {
+        const top = messageRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setMessageVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [messageVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (messageTextRef.current && !messageTextVisible) {
+        const top = messageTextRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setMessageTextVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [messageTextVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (designRef.current && !designVisible) {
+        const top = designRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setDesignVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [designVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (designTextRef.current && !designTextVisible) {
+        const top = designTextRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setDesignTextVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [designTextVisible]);
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (ideationRef.current && !ideationVisible) {
+        const top = ideationRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setIdeationVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [ideationVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (ideationTextRef.current && !ideationTextVisible) {
+        const top = ideationTextRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Adjust the threshold as needed
+        const threshold = 0.7;
+
+        if (top < windowHeight * threshold) {
+          setIdeationTextVisible(true);
+
+          // Remove the event listener once the animation is triggered
+          window.removeEventListener("scroll", handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [ideationTextVisible]);
   return (
     <div className="animated-bg">
       <div className="top-info">
@@ -32,22 +312,28 @@ const AnimatedText = () => {
         <ScrollText content={content} />
       </div>
       <div className="free-text">
-        <img
+        {/* <img
           data-aos="zoom-in-up"
           data-aos-duration="1000"
           src="/pitline.svg"
           alt=""
-        />
-        <p data-aos="zoom-in-down" 
-        data-aos-duration="2000"
-        className="animated-zoom animate-highlight">
+        /> */}
+        <p
+          ref={sectionRef}
+          className={`animated-text ${isVisible ? "animated-fade-down" : ""}`}
+        >
           Antidote is a zero-cost consulting agency for grassroots non-profit’s
           & progressive forces.
         </p>
       </div>
 
       <div className="services">
-        <button className="start">CORE SERVICES</button>
+        <button
+          ref={btnRef}
+          className={`start ${btnVisible ? "animated-fade-down-short" : ""}`}
+        >
+          CORE SERVICES
+        </button>
         <div className="mobile">
           <p>CORE</p>
           <p>SERVICES</p>
@@ -55,49 +341,85 @@ const AnimatedText = () => {
         <div className="line"></div>
 
         <div className="title-text">
-          <label htmlFor="" className="pressura title-label">
+          <label
+            htmlFor=""
+            ref={strategyRef}
+            className={`pressura title-label ${
+              strategyVisible ? "animated-fade-down-short" : ""
+            }`}
+          >
             STRATEGY
           </label>
-          <Bounce >
-          <p  className="animate-highlight">
+          <p
+            ref={strategyTextRef}
+            className={`animate-highlight ${
+              strategyTextVisible ? "animated-fade-down-texts" : ""
+            }`}
+          >
             Analyze needs and prioritize resources to establish a strong
             foundation for clear and focused progress.
           </p>
-          </Bounce>
         </div>
         <div className="title-text">
-          <label htmlFor="" className="pressura title-label">
+          <label
+            htmlFor=""
+            ref={messageRef}
+            className={`pressura title-label ${
+              messageVisible ? "animated-fade-down-short" : ""
+            }`}
+          >
             MESSAGING
           </label>
-          <Bounce >
-          <p  className="animate-highlight">
+          <p
+            ref={messageTextRef}
+            className={`animate-highlight ${
+              messageTextVisible ? "animated-fade-down-texts" : ""
+            }`}
+          >
             Distil messages in a ways that stick. Content that resonates beyond
             like-minded circles.
           </p>
-          </Bounce>
         </div>
 
         <div className="title-text">
-          <label htmlFor="" className="pressura title-label">
+          <label
+            htmlFor=""
+            ref={designRef}
+            className={`pressura title-label ${
+              designVisible ? "animated-fade-down-short" : ""
+            }`}
+          >
             DESIGN
           </label>
-          <Bounce >
-          <p  className="animate-highlight">
+          <p
+            ref={designTextRef}
+            className={`animate-highlight ${
+              designTextVisible ? "animated-fade-down-texts" : ""
+            }`}
+          >
             Confidence inspiring design. Flows and experiences that propel
             people to action.
           </p>
-          </Bounce>
         </div>
 
         <div className="title-text">
-          <label htmlFor="" className="pressura title-label">
+          <label
+            htmlFor=""
+            ref={ideationRef}
+            className={`pressura title-label ${
+              ideationVisible ? "animated-fade-down-short" : ""
+            }`}
+          >
             IDEATION
           </label>
-          <Bounce >
-          <p  className="animate-highlight">
+          <p
+            ref={ideationTextRef}
+            className={`animate-highlight ${
+              ideationTextVisible ? "animated-fade-down-texts" : ""
+            }`}
+          >
             Navigate roadblocks with brainstorming, idea building and iteration.
           </p>
-          </Bounce>
         </div>
       </div>
     </div>
